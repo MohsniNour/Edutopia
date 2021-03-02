14641e35f0e3249adc953ba96d15bdea2b543b39
MohsniNour <nourelhouda.mohsni@esprit.tn>
2021-03-02 02:26:01 +0100

    added add method to ActivityService class

--- src/Services/ActivityService.java
+++ src/Services/ActivityService.java
@@ -7,7 +7,12 @@ package Services;
 
 import Entities.Activity;
 import IServices.IActivity;
+import Utils.DataBaseConnection;
+import java.io.File;
 import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 /**
@@ -16,10 +21,29 @@ import java.util.List;
  */
 public class ActivityService implements IActivity{
     
+    Connection conn;
+
+    public ActivityService() {
+        conn = DataBaseConnection.getInstance().getConnection();
+    }
+    
 
     @Override
     public void add(Activity activity) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+         try {
+            String requete = "INSERT INTO activity (name,deadline,work_todo,id_course,created_by,ceated_date) VALUES(?,?,?,?,?,?)";
+
+            PreparedStatement pst = conn.prepareStatement(requete);
+            pst.setString(1, activity.getName());
+            pst.setDate(2, (Date) activity.getDeadline());
+            pst.setString(6, activity.getId_Course());
+            pst.setDate(7, (Date) activity.getCeated_date());
+            pst.executeUpdate();
+            
+        } catch (SQLException ex) {
+            System.err.println(ex.getMessage());
+        }
+
     }
 
     @Override
