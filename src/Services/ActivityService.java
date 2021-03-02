b6632ec7409fbf6b3ecafe65df2e88ccaee67774
MohsniNour <nourelhouda.mohsni@esprit.tn>
2021-03-02 01:47:38 +0100

    Created ActivityService.java

---
+++ src/Services/ActivityService.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Services;
+
+import Entities.Activity;
+import IServices.IActivity;
+import java.sql.Connection;
+import java.util.List;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class ActivityService implements IActivity{
+    
+
+    @Override
+    public void add(Activity activity) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void delete(Activity activity) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void edit(int id, Activity new_activity) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void details(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Activity> listAll() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
