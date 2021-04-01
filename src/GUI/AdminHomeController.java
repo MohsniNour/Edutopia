960f6791f3ef76cb7096930169abf18f9ad6abe8
Sabghine <sabrine.mokhtar@esprit.tn>
2021-04-01 22:38:40 +0100

    Dynamic statistics

---
+++ src/GUI/AdminHomeController.java
@@ -0,0 +1,146 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GUI;
+
+import Entities.User;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Dialog;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.util.Callback;
+
+/**
+ * FXML Controller class
+ *
+ * @author Sabrina
+ */
+public class AdminHomeController implements Initializable {
+
+    @FXML
+    private VBox vboxdrawer;
+    @FXML
+    private ImageView imagechange;
+    @FXML
+    private Label UserName;
+    @FXML
+    private Button btn_Course1;
+
+    /**
+     * Initializes the controller class.
+     */
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void HomeAction(ActionEvent event) throws IOException {
+         Parent root = FXMLLoader.load(getClass().getResource("AdminHome.fxml"));
+        Scene scene = new Scene(root);
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setScene(scene);
+        window.show();
+    }
+
+    @FXML
+    private void DepartmentAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void ClassAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void CourseAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void ExamAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void CalendarAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void Co_StudyingAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void AccountAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void ClaimAction(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("ComplaintAdd.fxml"));
+        Scene scene = new Scene(root);
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setScene(scene);
+        window.show();
+    }
+
+    @FXML
+    private void DisconnectionAction(ActionEvent event) {
+        Dialog confirmation = new Dialog();
+        GridPane grid2 = new GridPane();
+        Label l1 = new Label("Décnnecter?");
+        grid2.add(l1, 2, 2);
+        confirmation.setTitle("Confirmation de déconnection");
+        confirmation.getDialogPane().setContent(grid2);
+        ButtonType Confi = new ButtonType("Oui", ButtonBar.ButtonData.OK_DONE);
+        ButtonType Ann = new ButtonType("Non", ButtonBar.ButtonData.OK_DONE);
+        confirmation.getDialogPane().getButtonTypes().add(Confi);
+        confirmation.getDialogPane().getButtonTypes().add(ButtonType.CANCEL);
+        confirmation.setResultConverter(new Callback<ButtonType, User>() {
+            @Override
+            public User call(ButtonType param) {
+                if (param == Confi) {
+                    Parent root = null;
+                    try {
+                        root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+                    } catch (IOException ex) {
+                        Logger.getLogger(AdminHomeController.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                    Scene scene = new Scene(root);
+                    Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                    window.setScene(scene);
+                    window.show();
+                }
+
+                return null;
+            }
+        });
+        confirmation.showAndWait();
+    }
+
+    @FXML
+    private void UsersAction(ActionEvent event) throws IOException {
+         Parent root = FXMLLoader.load(getClass().getResource("UserAdd.fxml"));
+        Scene scene = new Scene(root);
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setScene(scene);
+        window.show();
+    }
+    
+}
