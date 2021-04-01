9d7c0642b6cb550071ee9355cb4df784510ac709
Sabghine <sabrine.mokhtar@esprit.tn>
2021-04-02 00:55:00 +0100

    Implemented AutoComplete API in the user search

--- src/GUI/UserAddController.java
+++ src/GUI/UserAddController.java
@@ -5,30 +5,43 @@
  */
 package GUI;
 
-import Entities.Activity;
+import Entities.Mail;
 import Entities.User;
-import Services.ActivityService;
 import Services.UserService;
+import java.io.IOException;
 import java.net.URL;
 import java.sql.Date;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Alert;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.Dialog;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.util.Callback;
+import org.controlsfx.control.textfield.TextFields;
 
 /**
  * FXML Controller class
@@ -36,19 +49,14 @@ import javafx.scene.layout.AnchorPane;
  * @author Sabrina
  */
 public class UserAddController implements Initializable {
+    
+    User currentUser = LoginController.CurrentUser;
 
     @FXML
     private TextField name;
     @FXML
     private TextField last_name;
-    @FXML
-    private ComboBox<String> role = new ComboBox();
-    ;
-    ObservableList<String> options = FXCollections.observableArrayList(
-            "Admin",
-            "Teacher",
-            "Student"
-    );
+    
     @FXML
     private TextField cin;
     @FXML
@@ -65,12 +73,9 @@ public class UserAddController implements Initializable {
     private TableColumn<User, String> l_name;
     @FXML
     private TableColumn<User, String> e_mail;
-    @FXML
-    private TableColumn<User, String> role_u;
+
     @FXML
     private TableView<User> user_t;
-    @FXML
-    private AnchorPane anchor_user;
     @FXML
     private Button edit;
     @FXML
@@ -81,15 +86,32 @@ public class UserAddController implements Initializable {
     private TextField search_txt;
     @FXML
     private Button search_btn;
+    @FXML
+    private TextField password;
+    @FXML
+    private VBox vboxdrawer;
+    @FXML
+    private ImageView imagechange;
+    @FXML
+    private Label UserName;
+    @FXML
+    private Button btn_Course1;
+  
+
 
     /**
      * Initializes the controller class.
+     * @param url
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        role.setItems(options);
-//        search_btn.setDisable(true);
         showUsers();
+        String[] possible = {"esprit.tn","yahoo.fr","gmail.com" } ;
+        TextFields.bindAutoCompletion(search_txt, possible);
+        
+     
+//        search_btn.setDisable(true);
+        
         
     }
 
@@ -100,17 +122,25 @@ public class UserAddController implements Initializable {
         f_name.setCellValueFactory(new PropertyValueFactory("first_name"));
         l_name.setCellValueFactory(new PropertyValueFactory("last_name"));
         e_mail.setCellValueFactory(new PropertyValueFactory("email"));
-        role_u.setCellValueFactory(new PropertyValueFactory("role"));
+        
+       
 
     }
 
     @FXML
     private void add_button_action(ActionEvent event) {
         UserService ss = new UserService();
-        User u = new User(cin.getText(), f_name.getText(), name.getText(), Date.valueOf(Birthdate.getValue()), phone_number.getText(), email.getText(), role.getValue().toString());
+        User u = new User(cin.getText(), f_name.getText(), name.getText(), Date.valueOf(Birthdate.getValue()), phone_number.getText(), email.getText(),password.getText(), "Admin", currentUser.getId());
         ss.addUser(u);
         showUsers();
+        try {
+            
+            Mail.sendMail(email.getText(), "Edutopia admin's password", "Welcome to Edutopia "+name.getText()+" this your password,save it somewhere safe :        "+password.getText());
+        } catch (Exception ex) {
+            Logger.getLogger(UserAddController.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
+    
 
     @FXML
     private void edit_user(ActionEvent event) {
@@ -119,7 +149,7 @@ public class UserAddController implements Initializable {
         u.setId(u.getId());
         u.setFirst_name(name.getText());
         u.setLast_name(last_name.getText());
-        u.setRole(role.getValue().toString());
+        u.setPassword(password.getText());
         System.out.println(u.getId());
         ds.UpdateUser(u.getId(), u);
         showUsers();
@@ -147,7 +177,99 @@ public class UserAddController implements Initializable {
             f_name.setCellValueFactory(new PropertyValueFactory("first_name"));
             l_name.setCellValueFactory(new PropertyValueFactory("last_name"));
             e_mail.setCellValueFactory(new PropertyValueFactory("email"));
-            role_u.setCellValueFactory(new PropertyValueFactory("role"));
+            
         
     }
-}
+
+    @FXML
+    private void HomeAction(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("AdminHome.fxml"));
+        Scene scene = new Scene(root);
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setScene(scene);
+        window.show();
+    }
+    
+
+    @FXML
+    private void DepartmentAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void ClassAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void CourseAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void ExamAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void CalendarAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void Co_StudyingAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void AccountAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void ClaimAction(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("ComplaintAdd.fxml"));
+        Scene scene = new Scene(root);
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setScene(scene);
+        window.show();
+    }
+
+    
+    @FXML
+    private void DisconnectionAction(ActionEvent event) {
+        Dialog confirmation = new Dialog();
+        GridPane grid2 = new GridPane();
+        Label l1 = new Label("Décnnecter?");
+        grid2.add(l1, 2, 2);
+        confirmation.setTitle("Confirmation de déconnection");
+        confirmation.getDialogPane().setContent(grid2);
+        ButtonType Confi = new ButtonType("Oui", ButtonBar.ButtonData.OK_DONE);
+        ButtonType Ann = new ButtonType("Non", ButtonBar.ButtonData.OK_DONE);
+        confirmation.getDialogPane().getButtonTypes().add(Confi);
+        confirmation.getDialogPane().getButtonTypes().add(ButtonType.CANCEL);
+        confirmation.setResultConverter(new Callback<ButtonType, User>() {
+            @Override
+            public User call(ButtonType param) {
+                if (param == Confi) {
+                    Parent root = null;
+                    try {
+                        root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+                    } catch (IOException ex) {
+                        Logger.getLogger(UserAddController.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                    Scene scene = new Scene(root);
+                    Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                    window.setScene(scene);
+                    window.show();
+                }
+
+                return null;
+            }
+        });
+        confirmation.showAndWait();
+    }
+
+    @FXML
+    private void UsersAction(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("UserAdd.fxml"));
+        Scene scene = new Scene(root);
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setScene(scene);
+        window.show();
+    }
+    }
+
